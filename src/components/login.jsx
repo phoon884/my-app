7ec2d87cc851d9166c82cd50f337bda4a2400b1d
phoon884 <67638051+phoon884@users.noreply.github.com>
2021-08-01 00:35:39 +0700

    react

---
+++ src/components/login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class login extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>inc</button>
+      </div>
+    );
+  }
+}
+
+export default login;
